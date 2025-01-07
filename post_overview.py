de6444a5dbaddf1a17ec26801806501cd0bddf39
bizbuddydev <bizbuddydev@gmail.com>
2025-01-06 21:15:10 -0800

    Update post_overview.py

--- post_overview.py
+++ post_overview.py
@@ -9,10 +9,10 @@ st.set_page_config(page_title="Post Analyzer", layout="wide", page_icon="📱")
 
 # Define links to other pages
 PAGES = {
-    "📊 Overview": "https://smp-bizbuddy-accountoverview.streamlit.app/",
-    "📱 Posts": "https://smp-bizbuddy-postoverview.streamlit.app",
-    "🗓️ Scheduler": "https://smp-bizbuddy-postscheduler.streamlit.app/",
-    "💡 Brainstorm": "https://smp-bizbuddy-v1-brainstorm.streamlit.app/"
+    "📊 Overview": "https://hv-bizbuddy-socialoverview.streamlit.app/",
+    "📱 Posts": "https://hv-bizbuddy-postoverview.streamlit.app",
+    "🗓️ Scheduler": "https://hv-bizbuddy-postscheduler.streamlit.app/",
+    "💡 Brainstorm": "https://hv-bizbuddy-v1-brainstorm.streamlit.app/"
 }
 
 # Sidebar navigation
